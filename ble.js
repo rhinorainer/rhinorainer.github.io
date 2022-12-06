1e280132a4df7ab6255088da5e8ab0e7556231b0
rhinorainer <119422268+rhinorainer@users.noreply.github.com>
2022-12-06 15:01:57 +0100

    Create ble.js

---
+++ ble.js
@@ -0,0 +1,29 @@
+const serviceUuid = "465A640E-7553-5DDC-86C9-B3E59919C36D";
+let myCharacteristic;
+let myValue = 0;
+let myBLE;
+
+function setup() {
+  // Create a p5ble class
+  myBLE = new p5ble();
+
+  // Create a 'Connect' button
+  const connectButton = createButton('Connect')
+  connectButton.mousePressed(connectToBle);
+}
+
+function connectToBle() {
+  // Connect to a device by passing the service UUID
+  //myBLE.connect(serviceUuid, gotCharacteristics);
+  
+  
+}
+
+// A function that will be called once got characteristics
+function gotCharacteristics(error, characteristics) {
+  console.log(error);  
+}
+
+function draw() {
+  
+}
